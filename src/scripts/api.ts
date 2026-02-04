4ac3996b6c9bbffd2a749fe1a8ccdc2805bb6786
goldbergz <zolotogornaya22@mail.ru>
2026-02-04 12:08:17 +0300

    feat: add api imitation

---
+++ src/scripts/api.ts
@@ -0,0 +1,13 @@
+import data from '../data.json' assert { type: 'json' };
+export interface Product {
+    id: number;
+    title: string;
+    price: number;
+    category: string;
+    image: string;
+    description: string;
+}
+
+export async function fetchProducts(): Promise<Product[]> {
+    return data;
+}
