38bbc816af95ce84dc08c1f4d4f63fe43e8947bd
goldbergz <zolotogornaya22@mail.ru>
2026-02-05 10:20:02 +0300

    feat: add search products

---
+++ src/scripts/form.ts
@@ -0,0 +1,24 @@
+import type { Product } from './api';
+import { renderProducts } from './card';
+
+export function initSearch(products: Product[], container: HTMLElement) {
+  const form = document.getElementById('search-form') as HTMLFormElement;
+  const input = form.querySelector('.search__input') as HTMLInputElement;
+
+  function handleSearch() {
+    const query = input.value.trim().toLowerCase();
+
+    const filtered = products.filter(product =>
+      product.title.toLowerCase().includes(query)
+    );
+
+    renderProducts(filtered, container);
+  }
+
+  input.addEventListener('input', handleSearch);
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    handleSearch();
+  });
+}
